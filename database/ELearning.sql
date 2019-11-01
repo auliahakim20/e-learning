6dd39ca98e06fb5c92327e1869c31f913b56c7a3
Agus Edy <edyagusc@gmail.com>
2019-11-01 20:41:36 +0700

    DB Update 20191101

--- database/ELearning.sql
+++ database/ELearning.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mariadb
--- Generation Time: Oct 18, 2019 at 12:48 PM
+-- Generation Time: Nov 01, 2019 at 01:39 PM
 -- Server version: 10.3.18-MariaDB-1:10.3.18+maria~bionic
 -- PHP Version: 7.2.22
 
@@ -184,10 +184,47 @@ CREATE TABLE `migration` (
 
 INSERT INTO `migration` (`version`, `apply_time`) VALUES
 ('m000000_000000_base', 1570628872),
+('m140209_132017_init', 1572096243),
+('m140403_174025_create_account_table', 1572096244),
+('m140504_113157_update_tables', 1572096246),
+('m140504_130429_create_token_table', 1572096247),
 ('m140506_102106_rbac_init', 1570628876),
+('m140830_171933_fix_ip_field', 1572096247),
+('m140830_172703_change_account_table_name', 1572096247),
+('m141222_110026_update_ip_field', 1572096248),
+('m141222_135246_alter_username_length', 1572096248),
+('m150614_103145_update_social_account_table', 1572096248),
+('m150623_212711_fix_username_notnull', 1572096248),
 ('m151024_072453_create_route_table', 1570628916),
+('m151218_234654_add_timezone_to_profile', 1572096248),
+('m160929_103127_add_last_login_at_to_user_table', 1572096249),
 ('m170907_052038_rbac_add_index_on_auth_assignment_user_id', 1570628876),
 ('m180523_151638_rbac_updates_indexes_without_prefix', 1570628876);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `public_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `gravatar_id` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `website` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `bio` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timezone` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`user_id`, `name`, `public_email`, `gravatar_email`, `gravatar_id`, `location`, `website`, `bio`, `timezone`) VALUES
+(1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -217,6 +254,115 @@ CREATE TABLE `route` (
   `status` smallint(6) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `route`
+--
+
+INSERT INTO `route` (`name`, `alias`, `type`, `status`) VALUES
+('/*', '*', '', 1),
+('/debug/*', '*', 'debug', 1),
+('/debug/default/*', '*', 'debug/default', 1),
+('/debug/default/db-explain', 'db-explain', 'debug/default', 1),
+('/debug/default/download-mail', 'download-mail', 'debug/default', 1),
+('/debug/default/index', 'index', 'debug/default', 1),
+('/debug/default/toolbar', 'toolbar', 'debug/default', 1),
+('/debug/default/view', 'view', 'debug/default', 1),
+('/debug/user/*', '*', 'debug/user', 1),
+('/debug/user/reset-identity', 'reset-identity', 'debug/user', 1),
+('/debug/user/set-identity', 'set-identity', 'debug/user', 1),
+('/gii/*', '*', 'gii', 1),
+('/gii/default/*', '*', 'gii/default', 1),
+('/gii/default/action', 'action', 'gii/default', 1),
+('/gii/default/diff', 'diff', 'gii/default', 1),
+('/gii/default/index', 'index', 'gii/default', 1),
+('/gii/default/preview', 'preview', 'gii/default', 1),
+('/gii/default/view', 'view', 'gii/default', 1),
+('/mimin/*', '*', 'mimin', 1),
+('/mimin/role/*', '*', 'mimin/role', 1),
+('/mimin/role/create', 'create', 'mimin/role', 1),
+('/mimin/role/delete', 'delete', 'mimin/role', 1),
+('/mimin/role/index', 'index', 'mimin/role', 1),
+('/mimin/role/permission', 'permission', 'mimin/role', 1),
+('/mimin/role/update', 'update', 'mimin/role', 1),
+('/mimin/role/view', 'view', 'mimin/role', 1),
+('/mimin/route/*', '*', 'mimin/route', 1),
+('/mimin/route/create', 'create', 'mimin/route', 1),
+('/mimin/route/delete', 'delete', 'mimin/route', 1),
+('/mimin/route/generate', 'generate', 'mimin/route', 1),
+('/mimin/route/index', 'index', 'mimin/route', 1),
+('/mimin/route/update', 'update', 'mimin/route', 1),
+('/mimin/route/view', 'view', 'mimin/route', 1),
+('/mimin/user/*', '*', 'mimin/user', 1),
+('/mimin/user/create', 'create', 'mimin/user', 1),
+('/mimin/user/delete', 'delete', 'mimin/user', 1),
+('/mimin/user/index', 'index', 'mimin/user', 1),
+('/mimin/user/update', 'update', 'mimin/user', 1),
+('/mimin/user/view', 'view', 'mimin/user', 1),
+('/site/*', '*', 'site', 1),
+('/site/about', 'about', 'site', 1),
+('/site/captcha', 'captcha', 'site', 1),
+('/site/contact', 'contact', 'site', 1),
+('/site/error', 'error', 'site', 1),
+('/site/index', 'index', 'site', 1),
+('/user/*', '*', 'user', 1),
+('/user/admin/*', '*', 'user/admin', 1),
+('/user/admin/assignments', 'assignments', 'user/admin', 1),
+('/user/admin/block', 'block', 'user/admin', 1),
+('/user/admin/confirm', 'confirm', 'user/admin', 1),
+('/user/admin/create', 'create', 'user/admin', 1),
+('/user/admin/delete', 'delete', 'user/admin', 1),
+('/user/admin/index', 'index', 'user/admin', 1),
+('/user/admin/info', 'info', 'user/admin', 1),
+('/user/admin/resend-password', 'resend-password', 'user/admin', 1),
+('/user/admin/switch', 'switch', 'user/admin', 1),
+('/user/admin/update', 'update', 'user/admin', 1),
+('/user/admin/update-profile', 'update-profile', 'user/admin', 1),
+('/user/create', 'create', 'user', 1),
+('/user/delete', 'delete', 'user', 1),
+('/user/index', 'index', 'user', 1),
+('/user/profile/*', '*', 'user/profile', 1),
+('/user/profile/index', 'index', 'user/profile', 1),
+('/user/profile/show', 'show', 'user/profile', 1),
+('/user/recovery/*', '*', 'user/recovery', 1),
+('/user/recovery/request', 'request', 'user/recovery', 1),
+('/user/recovery/reset', 'reset', 'user/recovery', 1),
+('/user/registration/*', '*', 'user/registration', 1),
+('/user/registration/confirm', 'confirm', 'user/registration', 1),
+('/user/registration/connect', 'connect', 'user/registration', 1),
+('/user/registration/register', 'register', 'user/registration', 1),
+('/user/registration/resend', 'resend', 'user/registration', 1),
+('/user/security/*', '*', 'user/security', 1),
+('/user/security/auth', 'auth', 'user/security', 1),
+('/user/security/login', 'login', 'user/security', 1),
+('/user/security/logout', 'logout', 'user/security', 1),
+('/user/settings/*', '*', 'user/settings', 1),
+('/user/settings/account', 'account', 'user/settings', 1),
+('/user/settings/confirm', 'confirm', 'user/settings', 1),
+('/user/settings/delete', 'delete', 'user/settings', 1),
+('/user/settings/disconnect', 'disconnect', 'user/settings', 1),
+('/user/settings/networks', 'networks', 'user/settings', 1),
+('/user/settings/profile', 'profile', 'user/settings', 1),
+('/user/update', 'update', 'user', 1),
+('/user/view', 'view', 'user', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `social_account`
+--
+
+CREATE TABLE `social_account` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `provider` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `client_id` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `data` text COLLATE utf8_unicode_ci DEFAULT NULL,
+  `code` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` int(11) DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -234,21 +380,51 @@ CREATE TABLE `subject` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `token`
+--
+
+CREATE TABLE `token` (
+  `user_id` int(11) NOT NULL,
+  `code` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` int(11) NOT NULL,
+  `type` smallint(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `token`
+--
+
+INSERT INTO `token` (`user_id`, `code`, `created_at`, `type`) VALUES
+(1, 'cWShCR3b_RbdZtZf7g5wPAPqIC-maX1W', 1572096302, 0);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  `auth_key` varchar(32) DEFAULT NULL,
-  `password_hash` varchar(255) NOT NULL,
-  `password_reset_token` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `status` smallint(6) NOT NULL DEFAULT 10,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `confirmed_at` int(11) DEFAULT NULL,
+  `unconfirmed_email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `blocked_at` int(11) DEFAULT NULL,
+  `registration_ip` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
   `created_at` int(11) NOT NULL,
   `updated_at` int(11) NOT NULL,
-  `verification_token` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `flags` int(11) NOT NULL DEFAULT 0,
+  `last_login_at` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `auth_key`, `confirmed_at`, `unconfirmed_email`, `blocked_at`, `registration_ip`, `created_at`, `updated_at`, `flags`, `last_login_at`) VALUES
+(1, 'admin', 'edyagusc@gmail.com', '$2y$10$zhgH9m9KtEHAP7UvpOiHaOkVwZNe12mjUslBauvKEyH9pL8q8Hv2e', 'Zex96X-pU8T4v6HnvKWkF5Ktp3gvD1VN', 1572096547, NULL, NULL, '172.22.0.1', 1572096302, 1572096302, 0, 1572613311);
 
 --
 -- Indexes for dumped tables
@@ -334,6 +510,12 @@ ALTER TABLE `migration`
   ADD PRIMARY KEY (`version`);
 
 --
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- Indexes for table `response_course`
 --
 ALTER TABLE `response_course`
@@ -347,19 +529,33 @@ ALTER TABLE `route`
   ADD PRIMARY KEY (`name`);
 
 --
+-- Indexes for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `account_unique` (`provider`,`client_id`),
+  ADD UNIQUE KEY `account_unique_code` (`code`),
+  ADD KEY `fk_user_account` (`user_id`);
+
+--
 -- Indexes for table `subject`
 --
 ALTER TABLE `subject`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `token`
+--
+ALTER TABLE `token`
+  ADD UNIQUE KEY `token_unique` (`user_id`,`code`,`type`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`),
-  ADD UNIQUE KEY `password_reset_token` (`password_reset_token`),
-  ADD UNIQUE KEY `email` (`email`);
+  ADD UNIQUE KEY `user_unique_username` (`username`),
+  ADD UNIQUE KEY `user_unique_email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -408,6 +604,12 @@ ALTER TABLE `response_course`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `social_account`
+--
+ALTER TABLE `social_account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `subject`
 --
 ALTER TABLE `subject`
@@ -417,7 +619,7 @@ ALTER TABLE `subject`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
@@ -473,10 +675,28 @@ ALTER TABLE `institution_instructor`
   ADD CONSTRAINT `FK_institution_instructor_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE;
 
 --
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `fk_user_profile` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
 -- Constraints for table `response_course`
 --
 ALTER TABLE `response_course`
   ADD CONSTRAINT `FK_response_course_course_lecture` FOREIGN KEY (`course_lecture_id`) REFERENCES `course_lecture` (`id`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `social_account`
+--
+ALTER TABLE `social_account`
+  ADD CONSTRAINT `fk_user_account` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `token`
+--
+ALTER TABLE `token`
+  ADD CONSTRAINT `fk_user_token` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
