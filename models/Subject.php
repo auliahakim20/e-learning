ed6f96e825b20c0c00405d47ccc63ecd65d0a3a8
Agus Edy <edyagusc@gmail.com>
2019-11-02 13:29:49 +0700

    added Subject

---
+++ models/Subject.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "subject".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $slug
+ * @property int $created_at
+ * @property int $updated_at
+ *
+ * @property Course[] $courses
+ */
+class Subject extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'subject';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['created_at', 'updated_at'], 'integer'],
+            [['name', 'slug'], 'string', 'max' => 100],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'slug' => Yii::t('app', 'Slug'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCourses()
+    {
+        return $this->hasMany(Course::className(), ['subject_id' => 'id']);
+    }
+}
