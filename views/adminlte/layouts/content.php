03b05d48f23cf36e76f12c21fa37f5c477109baf
Agus Edy <edyagusc@gmail.com>
2020-01-02 18:38:30 +0700

    updated copyright and version

--- views/adminlte/layouts/content.php
+++ views/adminlte/layouts/content.php
@@ -37,9 +37,9 @@ use dmstr\widgets\Alert;
 
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.0
+        <b>Version</b> <?= Yii::$app->version ?>
     </div>
-    <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    <strong>Copyright &copy; <?php echo '2019 - '.date('Y'); ?> <a href="<?= Yii::$app->homeUrl ?>">Team Adaptive Learning</a>.</strong> All rights
     reserved.
 </footer>
 
